e1c64dd2e593456259b17a7025389465b0be3513
SamGerber <gerbyxvi@gmail.com>
2016-04-13 22:20:51 -0700

    Add move history

--- bishop.rb
+++ bishop.rb
@@ -1,9 +1,9 @@
 require_relative 'sliding_piece'
 
 class Bishop < SlidingPiece
-  def initialize(color, position, board)
+  def initialize(color, position, board, move_history = nil)
     @directions = [[-1, -1], [1, 1], [-1, 1], [1, -1]]
-    super(color, position, @directions, board)
+    super(color, position, @directions, board, move_history)
   end
 
   def to_s
