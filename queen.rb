173222f60ef90de188490b354dc9b034d09507ac
SamGerber <gerbyxvi@gmail.com>
2016-04-13 22:29:51 -0700

    Add support for move history

--- queen.rb
+++ queen.rb
@@ -1,9 +1,9 @@
 require_relative 'sliding_piece'
 
 class Queen < SlidingPiece
-  def initialize(color, position, board)
+  def initialize(color, position, board, move_history = nil)
     @directions = [[-1, -1], [1, 1], [-1, 1], [1, -1], [-1, 0], [1, 0], [0, 1], [0, -1]]
-    super(color, position, @directions, board)
+    super(color, position, @directions, board, move_history)
   end
 
   def to_s
