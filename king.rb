c0bb375a56db8879de665e4746ee912c18a6ea3d
SamGerber <gerbyxvi@gmail.com>
2016-04-13 22:27:38 -0700

    Add support for move history and castling

--- king.rb
+++ king.rb
@@ -1,7 +1,7 @@
 require_relative 'stepping_piece'
 
 class King < SteppingPiece
-  def initialize(color, position, board)
+  def initialize(color, position, board, move_history = nil)
     @translations = [
       [-1, -1],
       [1, 1],
@@ -12,28 +12,37 @@ class King < SteppingPiece
       [0, 1],
       [0, -1]
     ]
-    super(color, position, @translations, board)
+    super(color, position, @translations, board, move_history)
   end
 
   def possible_moves
     moves = super
-    moves += castles
+    moves += castles if @board.current_player == color
+    moves
   end
 
   def castles
     return [] if has_moved
-    # return [] if @board.king_in_check?(color)
+    enemy_color = color == :white ? :black : :white
+    return [] if @board.can_attack?(position, enemy_color)
     castles = []
     rook_positions = [[position[0], 0], [position[0], 7]]
-    rook_positions.reject! { |position| @board[position].has_moved }
+    rook_positions.reject! do |position|
+      @board[position].has_moved || !@board[position].is_a?(Rook)
+    end
     rook_positions.each do |pos|
       if pos[1] > position[1]
-        castles << [pos[0], 6] if @board.empty?([pos[0], 5]) &&
-                                    @board.empty?([pos[0], 5])
+        castles << [pos[0], 6] if   @board.empty?([pos[0], 5]) &&
+                                    @board.empty?([pos[0], 6]) &&
+                                    !@board.can_attack?([pos[0], 5], enemy_color) &&
+                                    !@board.can_attack?([pos[0], 6], enemy_color)
       else
         castles << [pos[0], 2] if @board.empty?([pos[0], 1]) &&
                                     @board.empty?([pos[0], 2]) &&
-                                    @board.empty?([pos[0], 3])
+                                    @board.empty?([pos[0], 3]) &&
+                                    !@board.can_attack?([pos[0], 2], enemy_color) &&
+                                    !@board.can_attack?([pos[0], 3], enemy_color)
+
       end
     end
     castles
