a0d64da2bf25e0982210ed04f466b8c36658e658
SamGerber <gerbyxvi@gmail.com>
2016-04-13 22:30:02 -0700

    Add support for move history

--- rook.rb
+++ rook.rb
@@ -2,9 +2,9 @@ require_relative "sliding_piece"
 
 class Rook < SlidingPiece
 
-  def initialize(color, position, board)
+  def initialize(color, position, board, move_history = nil)
     @directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-    super(color, position, @directions, board)
+    super(color, position, @directions, board, move_history)
   end
 
   def to_s
