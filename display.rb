744049fe3c6260f6ce0e458146d04a66446887e4
SamGerber <gerbyxvi@gmail.com>
2016-04-13 22:23:20 -0700

    Various cosmetic changes

--- display.rb
+++ display.rb
@@ -3,17 +3,20 @@ require_relative 'cursorable'
 
 class Display
   include Cursorable
+  attr_reader :incrementer, :highlight
 
   def initialize(board)
     @board = board
     @cursor = [0, 0]
+    @incrementer = 0
+    @highlight = true
   end
 
   # Returns the board's grid as a string
   def build_grid
     header = [" ", " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "]
     grid_display = @board.grid.map.with_index do |row, index|
-      [index + 1] + build_row(row, index)
+      [8 - index] + build_row(row, index)
     end
     grid_display.unshift(header)
   end
@@ -37,32 +40,44 @@ class Display
       # mode = :blink
     # elsif @board.piece_in_hand.filter_moves.include?([i,j])
     #   bg = :white
+  elsif (i + j).odd?
+      bg = :white
+      bg = :green if highlight && @board.piece_in_hand.filter_moves.include?([i,j])
     elsif (i + j).even?
-      bg = @board.piece_in_hand.filter_moves.include?([i,j]) ? :green : :white
-    elsif (i + j).odd?
-      bg = @board.piece_in_hand.filter_moves.include?([i,j]) ? :light_green : :light_white
+      bg = :light_white
+      bg = :light_green if highlight && @board.piece_in_hand.filter_moves.include?([i,j])
     end
 
     { background: bg, color: :black, mode: mode } #@board[[i, j]].color
   end
 
   # Outputs the rendered board to STDOUT
-  def render
+  def render(promotion = false)
+    @highlight = false if promotion
     system("clear")
-    puts " #{build_piece_bank(:black)[0]}"
-    puts " #{build_piece_bank(:black)[1]}"
+    puts " #{build_piece_bank(:white)[0]}"
+    puts " #{build_piece_bank(:white)[1]}"
     build_grid.each { |row| puts row.join }
     puts footer
+    puts "Press TAB to choose".colorize(color: :red) if promotion
+    puts "Press and ENTER to confirm".colorize(color: :red) if promotion
+    @highlight = true
   end
 
   def footer
     footer = []
-    footer << " #{@board.current_player} is attempting to move #{@board.piece_in_hand.inspect}"
-    footer << " #{@board.current_player} King is in check!!!" if @board.king_in_check?(@board.current_player)
-    footer << " #{build_piece_bank(:white)[0]}"
-    footer << " #{build_piece_bank(:white)[1]}"
+    footer << " #{@board.current_player} is moving..."
+    footer << " #{@board.piece_in_hand.inspect} to #{beautify_position(@cursor)}" unless @board.piece_in_hand.is_a?(NullPiece)
+    footer << " #{@board.current_player} King is in check!!" if @board.king_in_check?(@board.current_player)
+    footer << " #{build_piece_bank(:black)[0]}"
+    footer << " #{build_piece_bank(:black)[1]}"
     # footer << @board.taken_pieces.join(" ")
     footer.join("\n")
+  end
+
+  def beautify_position(position)
+    letters = %w(a b c d e f g h)
+    "(#{letters[position[1]]}, #{8 - position[0]})"
   end
 
   def build_piece_bank(color)
@@ -73,7 +88,7 @@ class Display
     end
 
     pieces.each_with_index do |piece, i|
-      bank[i / 8][i] = piece
+      bank[i / 8][i - 8] = piece
     end
 
     bank.map do |row|
@@ -83,7 +98,7 @@ class Display
   end
 
   def bank_color_options(color)
-    bg = color == :white ? :black : :light_black
+    bg = color == :white ? :light_black : :light_white
     { background: bg, color: color}
   end
 
